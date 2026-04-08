f6599f40275e77ca730fc518013c87041be4b088
xaxxoo <bnahmad83@gmail.com>
2026-04-08 10:46:51 +0100

    AddWalletStatusToUsers1600000000001

--- src/database/migrations/1600000000001-AddWalletStatusToUsers.ts
+++ src/database/migrations/1600000000001-AddWalletStatusToUsers.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddWalletStatusToUsers1700000004 implements MigrationInterface {
-  name = 'AddWalletStatusToUsers1700000004';
+export class AddWalletStatusToUsers1600000000001 implements MigrationInterface {
+  name = 'AddWalletStatusToUsers1600000000001';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
