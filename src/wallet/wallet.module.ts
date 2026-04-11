96cc0c950aa8a7a21dec3f6daeb2a020afa87ebb
xaxxoo <bnahmad83@gmail.com>
2026-04-11 12:06:10 +0100

    feat(wallet): add WalletDepositScheduler to poll Stellar deposits every minute

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -7,11 +7,12 @@ import { TransactionsModule } from '../transactions/transactions.module'
 import { User } from '../auth/entities/user.entity'
 import { WalletController } from './wallet.controller'
 import { WalletService } from './wallet.service'
+import { WalletDepositScheduler } from './wallet.scheduler'
 
 @Module({
   imports:     [TypeOrmModule.forFeature([User]), BlockchainModule, RatesModule, TransactionsModule],
   controllers: [WalletController],
-  providers:   [WalletService],
+  providers:   [WalletService, WalletDepositScheduler],
   exports:     [WalletService],
 })
 export class WalletModule {}
